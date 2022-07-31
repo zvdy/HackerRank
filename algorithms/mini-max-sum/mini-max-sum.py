6b52ecbfa6a9c0cda67037b546c049400042ebb3
zvdy <cristian.030902@gmail.com>
2022-07-31 18:57:38 +0200

    Mini max sum

---
+++ algorithms/mini-max-sum/mini-max-sum.py
@@ -0,0 +1,26 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'miniMaxSum' function below.
+#
+# The function accepts INTEGER_ARRAY arr as parameter.
+#
+
+def miniMaxSum(arr):
+    # Write your code here
+    arr.sort()
+    min_sum = sum(arr[0:4])
+    max_sum = sum(arr[1:5])
+    print(min_sum, max_sum)
+
+if __name__ == '__main__':
+
+    arr = list(map(int, input().rstrip().split()))
+
+    miniMaxSum(arr)
