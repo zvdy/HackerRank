8e4cc16eb02d1933f14c3dcf9e90de5ec92839c8
zvdy <cristian.030902@gmail.com>
2022-07-31 19:27:11 +0200

    Reduced strings

---
+++ algorithms/Strings/reduced-strings/reduced-strings.md
@@ -0,0 +1,1 @@
+🖇️ https://www.hackerrank.com/challenges/reduced-string/problem
---
+++ algorithms/Strings/reduced-strings/reduced-strings.py
@@ -0,0 +1,41 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'superReducedString' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def superReducedString(s):
+    # Write your code here
+    stack = []
+    for i in s:
+        if len(stack) == 0:
+            stack.append(i)
+        else:
+            if stack[-1] == i:
+                stack.pop()
+            else:
+                stack.append(i)
+    if len(stack) == 0:
+        return "Empty String"
+    else:
+        return "".join(stack)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = superReducedString(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
