9f0528303ccaa432de7f42b4415002f8ad6ce931
zvdy <cristian.030902@gmail.com>
2022-07-31 17:37:30 +0200

    Big Sum & Diagonal difference

---
+++ algorithms/a-very-big-sum/a-very-big-sum.md
@@ -0,0 +1,16 @@
+In this challenge, you are required to calculate and print the sum of the elements in an array, keeping in mind that some of those integers may be quite large.
+
+Function Description
+
+Complete the aVeryBigSum function in the editor below. It must return the sum of all array elements.
+
+aVeryBigSum has the following parameter(s):
+
+int ar[n]: an array of integers .
+
+Return
+long: the sum of all array elements
+
+Input Format
+The first line of the input consists of an integer n.
+The next line contains n space-separated integers contained in the array.
---
+++ algorithms/diagonal-difference/diagonal-difference.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'diagonalDifference' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts 2D_INTEGER_ARRAY arr as parameter.
+#
+
+def diagonalDifference(arr):
+    # Write your code here
+    left_to_right = 0
+    right_to_left = 0
+    for i in range(len(arr)):
+        left_to_right += arr[i][i]
+        right_to_left += arr[i][len(arr)-i-1]
+    return abs(left_to_right - right_to_left)
+    
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    arr = []
+
+    for _ in range(n):
+        arr.append(list(map(int, input().rstrip().split())))
+
+    result = diagonalDifference(arr)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
