28b06c9606bb60018a28ee72dc5ad1641d9caff8
zvdy <cristian.030902@gmail.com>
2022-08-01 16:27:57 +0200

    Panagram

---
+++ algorithms/Strings/pangrams/pangrams.py
@@ -0,0 +1,34 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'pangrams' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts STRING s as parameter.
+#
+
+def pangrams(s):
+    # Write your code here
+    s = s.lower()
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    for i in alphabet:
+        if i not in s:
+            return 'not pangram'
+    return 'pangram'
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = pangrams(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
