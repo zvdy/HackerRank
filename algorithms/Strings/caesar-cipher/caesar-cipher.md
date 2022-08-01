673875d79e2903d9eea0db2430378281bf97f79a
zvdy <cristian.030902@gmail.com>
2022-08-01 15:43:33 +0200

    Caesar cipher

---
+++ algorithms/Strings/caesar-cipher/caesar-cipher.md
@@ -0,0 +1,1 @@
+🖇️ https://www.hackerrank.com/challenges/caesar-cipher-1/problem
---
+++ algorithms/Strings/caesar-cipher/caesar-cipher.py
@@ -0,0 +1,43 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'caesarCipher' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING s
+#  2. INTEGER k
+#
+
+def caesarCipher(s, k):
+    # Write your code here
+    k = k % 26
+    s = list(s)
+    for i in range(len(s)):
+        if s[i].isalpha():
+            if s[i].isupper():
+                s[i] = chr((ord(s[i]) + k - 65) % 26 + 65)
+            else:
+                s[i] = chr((ord(s[i]) + k - 97) % 26 + 97)
+    return ''.join(s)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    s = input()
+
+    k = int(input().strip())
+
+    result = caesarCipher(s, k)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
